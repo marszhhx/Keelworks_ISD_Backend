007270aea1384cb86ca23c528ebbf867959f5e9f
davidpascual2 <davidpascual200@gmail.com>
2023-12-11 15:04:11 -0800

    added tables to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,69 @@
+
 DROP DATABASE IF EXISTS isd_db;
 
 CREATE DATABASE isd_db;
+
+USE isd_db;
+
+DROP TABLE IF EXISTS new_request_form, needs_analysis, objective, final_assessment_strat, course_structure, course_strategy_document, storyboard;
+
+CREATE TABLE new_request_form(
+    submission_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(255),
+    problem_statement TEXT,
+    problem_data TEXT,
+    value_of_change TEXT,
+    people_needed_to_attend INT,
+    expected_growth TEXT,
+    priority_urgency TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE needs_analysis(
+    submission_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(255),
+    problem_data VARCHAR(50),
+    problem_statement VARCHAR(50),
+    success_statement TEXT,
+    audience_definition TEXT,
+    audience_benefits TEXT,
+    audience_needs TEXT,
+    change_issues TEXT,
+    tech_issues TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE objective(
+    terminal_objective TEXT,
+    enabling_objective TEXT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE final_assesment_strat(
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE course_structure(
+    module_heading VARCHAR(50),
+    lesson_heading VARCHAR(50),
+    lesson_details VARCHAR(50),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE course_strategy_document(
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE storyboard(
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
