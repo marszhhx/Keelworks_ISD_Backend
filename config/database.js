e9da4f90fd0a27d8c271dc234afaa1f52486fb0f
davidpascual2 <davidpascual200@gmail.com>
2023-12-11 13:59:50 -0800

    created schema and connected to database

--- config/database.js
+++ config/database.js
@@ -1,43 +1,5 @@
-// const Sequelize = require('sequelize');
-// const loadModels = require('../models');
-
-// let sequelize;
-
-// if (process.env.NODE_ENV === 'production') {
-// 	// Set up your production database connection details here
-// 	sequelize = new Sequelize({
-// 		dialect: 'postgres',
-// 		host: process.env.DB_HOST,
-// 		username: process.env.DB_USER,
-// 		password: process.env.DB_PASSWORD,
-// 		database: process.env.DB_NAME,
-// 	});
-// } else {
-// 	// Use SQLite for development
-// 	sequelize = new Sequelize({
-// 		dialect: 'sqlite',
-// 		storage: __dirname + '/../dev_database/dev_database.sqlite',
-// 	});
-// }
-
-// // const models = loadModels(sequelize);
-
-// sequelize
-// 	.sync({ force: false })
-// 	.then(() => {
-// 		console.log('Database and tables created!');
-
-// 		return sequelize.authenticate();
-// 	})
-// 	.catch(err => {
-// 		console.error('Error creating database and tables:', err);
-// 	});
-
-// module.exports = { sequelize, ...models };
-
-//===============================================
-
 const Sequelize = require('sequelize');
+require('dotenv').config();
 // const loadModels = require('../models');
 
 let sequelize;
@@ -52,11 +14,17 @@ if (process.env.NODE_ENV === 'production') {
 		database: process.env.DB_NAME,
 	});
 } else {
-	// Use SQLite for development
-	sequelize = new Sequelize({
-		dialect: 'sqlite',
-		storage: __dirname + '/../dev_database/dev_database.sqlite',
-	});
+	// Use mySQL for development
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: '127.0.0.1',
+            dialect: 'mysql',
+            port: 3306
+        }
+	);
 }
 
 // const models = loadModels(sequelize);
