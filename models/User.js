d16ac9b9de50954a502ec9b87b9c52c9db8ce9b8
davidpascual2 <davidpascual200@gmail.com>
2023-12-21 20:43:49 -0800

    removed duplicate code

--- models/User.js
+++ models/User.js
@@ -40,51 +40,10 @@ User.init(
   }
 );
 
-// User.associate = (models) => {
-//   User.hasMany(models.Member, {
-//     foreignKey: 'member_id',
-//   });
-// };
-
-//=============================
-
-// let sequelize;
-
-// const User = providedSequelize => {
-// 	sequelize = providedSequelize || require('../config/database');
-
-// 	const UserModel = sequelize.define('User', {
-// 		user_id: {
-// 			type: DataTypes.INTEGER,
-// 			allowNull: false,
-// 			primaryKey: true,
-// 			autoIncrement: true,
-// 		},
-// 		name: {
-// 			type: DataTypes.STRING,
-// 			allowNull: false,
-// 		},
-// 		email: {
-// 			type: DataTypes.STRING,
-// 			allowNull: false,
-// 			unique: true,
-// 			// validate: {
-// 			// 	isEmail: true,
-// 			// }
-// 		},
-// 		password: {
-// 			type: DataTypes.STRING,
-// 			allowNull: false,
-// 		},
-// 	});
-
-// 	UserModel.associate = models => {
-// 		UserModel.hasMany(models.Member, {
-// 			foreignKey: 'member_id',
-// 		});
-// 	};
-
-// 	return UserModel;
-// };
+User.associate = (models) => {
+  User.hasMany(models.Member, {
+    foreignKey: 'member_id',
+  });
+};
 
 module.exports = User;
