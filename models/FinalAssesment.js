87eba57191d6add89a6900853f6724ca92b7d0d2
Hongkun LIU <66795576+lhkun123@users.noreply.github.com>
2024-01-22 12:46:38 -0800

    create tables with sequelize

--- models/FinalAssesment.js
+++
@@ -1,3 +0,0 @@
-const { DataTypes } = require('sequelize');
-
-let sequelize;
--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 let sequelize;
 
 const User = providedSequelize => {
